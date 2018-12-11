6a8485fba9f99bffda835ee3ef920d7cfa97a1d5
= <=>
2018-12-10 21:17:29 -0500

    Flask

---
+++ TextSearch/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import mongo_stuff
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("main.html")
+
+@app.route('/search')
+def search():
+    search_query = request.args.get("query")
+    messages = mongo_stuff.search_get_messages(search_query)
+    sorted_messages = mongo_stuff.sorted_messages(messages, search_query)
+    print(sorted_messages)
+    return render_template("search.html", message_list = sorted_messages)
